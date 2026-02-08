7582ef9a16bbb0d8dd19566dec2ea891b37aa387
revssss1 <karl.alexis2803@gmail.com>
2026-02-08 21:31:42 +0800

    initial commit

---
+++ src/Hero.vue
@@ -0,0 +1,22 @@
+<script>
+    import { ref } from 'vue'
+
+
+</script>
+
+<template>
+    <nav>
+        <div>
+
+        </div>
+    </nav>
+
+</template>
+
+<style>
+    .nav_container {
+        displayf
+        padding: 8px,
+        
+    }
+</style>
